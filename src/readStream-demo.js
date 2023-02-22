c5f77c6cd7ee55cf9caf7df0e1345139a4ef00a2
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-22 21:35:28 +0530

    read stream demo

---
+++ src/readStream-demo.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const main = async () => {
+    const readStream = fs.createReadStream('../data/mock-data.csv', {highWaterMark: 100});
+
+    readStream.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    readStream.on('error', () => {
+        console.error('Error in file reading');
+    });
+
+    readStream.on('end', () => {
+        console.log('stream ended with success');
+    });
+}
+
+main();

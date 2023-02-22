c9361cc698a11bbde2428bb6bfe31d1af59efdb4
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-22 06:08:59 +0530

    created fack data

---
+++ src/create-data.js
@@ -0,0 +1,24 @@
+const { faker } =  require('@faker-js/faker');
+const { createWriteStream } = require('fs');
+
+const writeStream = createWriteStream('./data/mock-data.csv');
+writeStream.write('name;email;age;salary;isActive\n');
+
+for (let index = 0; index < 10000; index++) {
+    const firstName = faker.name.firstName();
+    const email = faker.internet.email(firstName);
+    const age = faker.datatype.number({ min: 10, max: 100 });
+    const salary = faker.random.numeric(4, { allowLeadingZeros: true })
+    const active = faker.datatype.boolean()
+
+    const arr = [
+        firstName,
+        email,
+        age,
+        salary,
+        active
+    ]
+    writeStream.write(arr.join(';') + '\n')
+}
+
+writeStream.end();
